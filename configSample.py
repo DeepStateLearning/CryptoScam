f9daa62cb8e5d7875460d2b809d89ed6c9cffe17
DeepStateLearning <40159109+DeepStateLearning@users.noreply.github.com>
2018-06-10 22:24:18 -0700

    Add files via upload

---
+++ configSample.py
@@ -0,0 +1,11 @@
+#--------------------------------------------------------------------------------
+# These tokens are needed for user authentication.
+# Credentials can be generates via Twitter's Application Management:
+#	https://apps.twitter.com/app/new
+#--------------------------------------------------------------------------------
+consumer_key = "0...............bY2SjPNssO8K"
+consumer_secret = "1F.............................................Rp"
+access_key = "359............................................dXp"
+access_secret = "tt............................................y"
+
+
